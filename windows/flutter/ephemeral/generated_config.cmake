d0c3aea8565aa8576147a9385c4576aabd30e101
JFXY <78674605+JoceFoo@users.noreply.github.com>
2023-03-20 21:32:58 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\User\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\User\\Namer_app\\namer_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\User\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\User\\Namer_app\\namer_app"
+  "FLUTTER_ROOT=C:\\Users\\User\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\User\\Namer_app\\namer_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\User\\Namer_app\\namer_app"
+  "FLUTTER_TARGET=C:\\Users\\User\\Namer_app\\namer_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\User\\Namer_app\\namer_app\\.dart_tool\\package_config.json"
+)
